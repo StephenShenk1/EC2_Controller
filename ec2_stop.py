4c1d17754c709a535f193603bc0b9da8cfcfc2ae
ankit shukla <37360428+ankit20000@users.noreply.github.com>
2025-06-28 16:39:55 +0530

    Create ec2_stop.py

---
+++ ec2_stop.py
@@ -0,0 +1,26 @@
+import boto3
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name='ap-south-1')  
+    filters = [{
+        'Name': 'tag:AutoManage',
+        'Values': ['True']
+    }]
+
+    response = ec2.describe_instances(Filters=filters)
+    instances_to_stop = [
+        instance['InstanceId']
+        for reservation in response['Reservations']
+        for instance in reservation['Instances']
+        if instance['State']['Name'] == 'running'
+    ]
+
+    if instances_to_stop:
+        print(f"Stopping instances: {instances_to_stop}")
+        ec2.stop_instances(InstanceIds=instances_to_stop)
+    else:
+        print("No instances to stop.")
+
+    return {
+        'StoppedInstances': instances_to_stop
+    }
